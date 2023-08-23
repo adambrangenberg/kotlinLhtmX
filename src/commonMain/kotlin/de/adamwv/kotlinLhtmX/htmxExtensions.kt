badc6a5412b7a32630dbbd09e0b6026466490eeb
adambrangenberg <adambrangenberg@proton.me>
2023-08-23 16:36:27 +0200

    Completing Additional Stuff and formatting

--- src/commonMain/kotlin/de/adamwv/kotlinLhtmX/htmxExtensions.kt
+++ src/commonMain/kotlin/de/adamwv/kotlinLhtmX/htmxExtensions.kt
@@ -7,7 +7,8 @@ import kotlinx.html.script
 private var deeznuts = false
 private fun deeznutsInYourMouth(functionName: String): Boolean {
     if (!deeznuts) {
-        throw Exception("Cannot use $functionName without including htmx")
+        throw Exception(
+            "Cannot use $functionName without including htmx. Use the includeHtmx function inside head to do so.")
     }
 
     return true
@@ -46,13 +47,11 @@ fun HTMLTag.hxBoost(value: String) {
     }
 }
 
-
 fun HTMLTag.hxPost(value: String) {
     if (deeznutsInYourMouth("hx-post")) {
         attributes += Pair("hx-post", value)
     }
 }
-
 
 fun HTMLTag.hxOn(value: String) {
     if (deeznutsInYourMouth("hx-on")) {
@@ -60,13 +59,11 @@ fun HTMLTag.hxOn(value: String) {
     }
 }
 
-
 fun HTMLTag.hxPushUrl(value: String) {
     if (deeznutsInYourMouth("hx-push-url")) {
         attributes += Pair("hx-push-url", value)
     }
 }
-
 
 fun HTMLTag.hxSelect(value: String) {
     if (deeznutsInYourMouth("hx-select")) {
@@ -74,13 +71,11 @@ fun HTMLTag.hxSelect(value: String) {
     }
 }
 
-
 fun HTMLTag.hxSelectObb(value: String) {
     if (deeznutsInYourMouth("hx-select-obb")) {
         attributes += Pair("hx-select-obb", value)
     }
 }
-
 
 fun HTMLTag.hxSwapObb(value: String) {
     if (deeznutsInYourMouth("hx-swap-obb")) {
@@ -88,16 +83,154 @@ fun HTMLTag.hxSwapObb(value: String) {
     }
 }
 
-
 fun HTMLTag.hxTrigger(value: String) {
     if (deeznutsInYourMouth("hx-trigger")) {
         attributes += Pair("hx-trigger", value)
     }
 }
 
-
 fun HTMLTag.hxVals(value: String) {
     if (deeznutsInYourMouth("hx-vals")) {
         attributes += Pair("hx-vals", value)
+    }
+}
+
+// Additional Attributes
+fun HTMLTag.hxConfirm(value: String) {
+    if (deeznutsInYourMouth("hx-confirm")) {
+        attributes += Pair("hx-confirm", value)
+    }
+}
+
+fun HTMLTag.hxDelete(value: String) {
+    if (deeznutsInYourMouth("hx-delete")) {
+        attributes += Pair("hx-delete", value)
+    }
+}
+
+fun HTMLTag.hxDisable(value: String) {
+    if (deeznutsInYourMouth("hx-disable")) {
+        attributes += Pair("hx-disable", value)
+    }
+}
+
+fun HTMLTag.hxDisinherit(value: String) {
+    if (deeznutsInYourMouth("hx-disinherit")) {
+        attributes += Pair("hx-disinherit", value)
+    }
+}
+
+fun HTMLTag.hxEncoding(value: String) {
+    if (deeznutsInYourMouth("hx-encoding")) {
+        attributes += Pair("hx-encoding", value)
+    }
+}
+
+fun HTMLTag.hxExt(value: String) {
+    if (deeznutsInYourMouth("hx-ext")) {
+        attributes += Pair("hx-ext", value)
+    }
+}
+
+fun HTMLTag.hxHeaders(value: String) {
+    if (deeznutsInYourMouth("hx-headers")) {
+        attributes += Pair("hx-headers", value)
+    }
+}
+
+fun HTMLTag.hxHistory(value: String) {
+    if (deeznutsInYourMouth("hx-history")) {
+        attributes += Pair("hx-history", value)
+    }
+}
+
+fun HTMLTag.hxHistoryElt(value: String) {
+    if (deeznutsInYourMouth("hx-history-elt")) {
+        attributes += Pair("hx-history-elt", value)
+    }
+}
+
+fun HTMLTag.hxInclude(value: String) {
+    if (deeznutsInYourMouth("hx-include")) {
+        attributes += Pair("hx-include", value)
+    }
+}
+
+fun HTMLTag.hxIndicator(value: String) {
+    if (deeznutsInYourMouth("hx-indicator")) {
+        attributes += Pair("hx-indicator", value)
+    }
+}
+
+fun HTMLTag.hxParams(value: String) {
+    if (deeznutsInYourMouth("hx-params")) {
+        attributes += Pair("hx-params", value)
+    }
+}
+
+fun HTMLTag.hxPatch(value: String) {
+    if (deeznutsInYourMouth("hx-patch")) {
+        attributes += Pair("hx-patch", value)
+    }
+}
+
+fun HTMLTag.hxPreserve(value: String) {
+    if (deeznutsInYourMouth("hx-preserve")) {
+        attributes += Pair("hx-preserve", value)
+    }
+}
+
+fun HTMLTag.hxPrompts(value: String) {
+    if (deeznutsInYourMouth("hx-prompt")) {
+        attributes += Pair("hx-prompt", value)
+    }
+}
+
+fun HTMLTag.hxPut(value: String) {
+    if (deeznutsInYourMouth("hx-put")) {
+        attributes += Pair("hx-put", value)
+    }
+}
+
+fun HTMLTag.hxReplaceUrl(value: String) {
+    if (deeznutsInYourMouth("hx-replace-url")) {
+        attributes += Pair("hx-replace-url", value)
+    }
+}
+
+fun HTMLTag.hxRequest(value: String) {
+    if (deeznutsInYourMouth("hx-request")) {
+        attributes += Pair("hx-request", value)
+    }
+}
+
+fun HTMLTag.hxSse(value: String) {
+    if (deeznutsInYourMouth("hx-sse")) {
+        attributes += Pair("hx-sse", value)
+    }
+}
+
+fun HTMLTag.hxSync(value: String) {
+    if (deeznutsInYourMouth("hx-sync")) {
+        attributes += Pair("hx-sync", value)
+    }
+}
+
+fun HTMLTag.hxValidate(value: String) {
+    if (deeznutsInYourMouth("hx-validate")) {
+        attributes += Pair("hx-validate", value)
+    }
+}
+
+@Deprecated("Please use hxVals(String): Unit instead")
+fun HTMLTag.hxVars(value: String) {
+    if (deeznutsInYourMouth("hx-vars")) {
+        attributes += Pair("hx-vars", value)
+    }
+}
+
+fun HTMLTag.hxWs(value: String) {
+    if (deeznutsInYourMouth("hx-ws")) {
+        attributes += Pair("hx-ws", value)
     }
 }
